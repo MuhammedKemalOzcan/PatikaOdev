74c8e603fbc57ec91a383facd272e0fd23947cb4
mami_ <mami_@DESKTOP-H9BT6T7>
2024-02-27 14:42:29 +0300

    Add project files.

---
+++ Uygulama1/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace PatikaOdev1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Dizi boyutunu belirtiniz:");
+            int diziBoyutu = int.Parse(Console.ReadLine());
+            int[] sayilar = new int[diziBoyutu];
+
+            for (int i = 0; i < diziBoyutu; i++)
+            {
+                Console.Write("Dizinin {0}. Elemanını giriniz:",i+1);
+                sayilar[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (int i in sayilar)
+            {
+                if (i % 2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+
+        }
+    }
+}
